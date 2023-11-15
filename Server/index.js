7c27af6791e8c0ee66246d47722c3641a7f26e93
Duan Antonio Espinoza Olivares <75913012+Duan-Espinoza@users.noreply.github.com>
2023-11-14 23:38:49 -0600

    arreglo del sv
    sv de socket.io a express

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,25 @@
+const express = require('express');
 const http = require('http');
-const server = http.createServer();
-const io = require('socket.io')(server, {
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
     cors: { origin: '*' },
 });
 
-io.on('connection', (socket) => { 
-    console.log('A user connected');
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-server.listen(3000);
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    // Aquí puedes agregar lógica adicional para gestionar eventos de Socket.IO
+    // Por ejemplo, escuchar eventos 'disconnect', 'message', etc.
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
